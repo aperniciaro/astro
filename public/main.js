0b8341240ced7793942ec02e07e8a7797b2f74ba
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-13 16:48:18 -0500

    dynamically loading pic of the day as background

--- public/main.js
+++ public/main.js
@@ -1,3 +1,15 @@
-const main = () => {}
+const getBackground = () => {
+  fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
+    .then(resp => {
+      return resp.json()
+    })
+    .then(potd => {
+      todayPotd = potd
+      console.log(todayPotd.hdUrl)
+      document.getElementById('pic-of-the-day').style.backgroundImage = `url(${
+        todayPotd.hdUrl
+      })`
+    })
+}
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', getBackground)
