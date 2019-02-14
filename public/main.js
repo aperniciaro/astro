60feaf02aa7f8c4e435a4e1d2549b7a809a0e1be
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-13 21:12:25 -0500

    get copyright info from api

--- public/main.js
+++ public/main.js
@@ -1,14 +1,17 @@
+let todayPotd = {}
+
 const getBackground = () => {
   fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
     .then(resp => {
       return resp.json()
     })
     .then(potd => {
-      todayPotd = potd
-      console.log(todayPotd.hdUrl)
+      // todayPotd = potd
       document.getElementById('pic-of-the-day').style.backgroundImage = `url(${
-        todayPotd.hdUrl
+        potd.hdUrl
       })`
+      let picMeta = 'copyright: ' + potd.copyright + ' | title: ' + potd.title
+      document.querySelector('.copyright-text').textContent = picMeta
     })
 }
 
