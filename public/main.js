dfa4dcb98f009eb2c8289fb21893ab25f88ff996
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-15 09:59:56 -0500

    adding render function

--- public/main.js
+++ public/main.js
@@ -35,11 +35,80 @@ const navigate = direction => {
     }
   }
   console.log(launchIndex)
-  render(launchArray[launchIndex])
+  render(launchIndex)
 }
 
 const render = index => {
-  //output launch-data section
+  const parent = document.querySelector('.launch-data')
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
+  }
+  //launch name
+  const nameElement = document.createElement('section')
+  nameElement.classList.add('launch-element')
+  nameElement.classList.add('name-element')
+  parent.appendChild(nameElement)
+
+  const nameLogo = document.createElement('i')
+  nameLogo.classList.add('fas fa-space-shuttle')
+  nameElement.appendChild(nameLogo)
+
+  const nameText = document.createElement('h3')
+  nameText.classList.add('launch-name')
+  nameElement.appendChild(nameText)
+  //launch descr
+  const descriptionElement = document.createElement('section')
+  descriptionElement.classList.add('launch-element')
+  descriptionElement.classList.add('description-element')
+  parent.appendChild(descriptionElement)
+
+  const descriptionLogo = document.createElement('i')
+  descriptionLogo.classList.add('fas fa-info-circle')
+  descriptionElement.appendChild(descriptionLogo)
+
+  const descriptionText = document.createElement('p')
+  descriptionText.classList.add('launch-description')
+  descriptionElement.appendChild(descriptionText)
+
+  const nameElement = document.createElement('section')
+  nameElement.classList.add('launch-element')
+  nameElement.classList.add('name-element')
+  parent.appendChild(nameElement)
+
+  const nameLogo = document.createElement('i')
+  nameLogo.classList.add('fas fa-space-shuttle')
+  nameElement.appendChild(nameLogo)
+
+  const nameText = document.createElement('h3')
+  nameText.classList.add('launch-name')
+  nameElement.appendChild(nameText)
+
+  const nameElement = document.createElement('section')
+  nameElement.classList.add('launch-element')
+  nameElement.classList.add('name-element')
+  parent.appendChild(nameElement)
+
+  const nameLogo = document.createElement('i')
+  nameLogo.classList.add('fas fa-space-shuttle')
+  nameElement.appendChild(nameLogo)
+
+  const nameText = document.createElement('h3')
+  nameText.classList.add('launch-name')
+  nameElement.appendChild(nameText)
+
+  //   <section class="launch-element">
+  //     <i class="fas fa-info-circle"></i>
+  //     <p class="launch-description">launch description</p>
+  //   </section>
+  //   <section class="launch-element">
+  //     <i class="fas fa-clock"></i>
+  //     <p class="launch-time">countdown</p>
+  //   </section>
+  //   <section class="launch-element">
+  //     <i class="fas fa-map-marked-alt"></i>
+  //     <p class="launch-location">launch location</p>
+  //   </section>
+  // </section>
 }
 
 const getBackground = () => {
