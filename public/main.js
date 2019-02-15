28f9c91e55ccbda2e408509233b2a9a2018bef92
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-15 12:07:22 -0500

    fixed getlaunch function

--- public/main.js
+++ public/main.js
@@ -19,85 +19,6 @@ const main = () => {
   render(launchIndex)
 }
 
-const navigate = direction => {
-  console.log(direction)
-  if (direction === 'right') {
-    if (launchIndex === launchArray.length - 1) {
-      launchIndex = 0
-    } else {
-      launchIndex++
-    }
-  } else if (direction === 'left') {
-    if (launchIndex === 0) {
-      launchIndex = launchArray.length - 1
-    } else {
-      launchIndex--
-    }
-  }
-  console.log(launchIndex)
-  render(launchIndex)
-}
-
-const render = index => {
-  const parent = document.querySelector('.launch-data')
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild)
-  }
-
-  //launch name
-  const nameElement = document.createElement('section')
-  nameElement.classList.add('launch-element')
-  nameElement.classList.add('name-element')
-  parent.appendChild(nameElement)
-
-  const nameLogo = document.createElement('i')
-  nameLogo.classList.add('fas fa-space-shuttle')
-  nameElement.appendChild(nameLogo)
-
-  const nameText = document.createElement('h3')
-  nameText.classList.add('launch-name')
-  nameElement.appendChild(nameText)
-  //launch descr
-  const descriptionElement = document.createElement('section')
-  descriptionElement.classList.add('launch-element')
-  descriptionElement.classList.add('description-element')
-  parent.appendChild(descriptionElement)
-
-  const descriptionLogo = document.createElement('i')
-  descriptionLogo.classList.add('fas fa-info-circle')
-  descriptionElement.appendChild(descriptionLogo)
-
-  const descriptionText = document.createElement('p')
-  descriptionText.classList.add('launch-description')
-  descriptionElement.appendChild(descriptionText)
-  //launch time
-  const timeElement = document.createElement('section')
-  timeElement.classList.add('launch-element')
-  timeElement.classList.add('time-element')
-  parent.appendChild(timeElement)
-
-  const timeLogo = document.createElement('i')
-  timeLogo.classList.add('fas fa-clock')
-  timeElement.appendChild(timeLogo)
-
-  const timeText = document.createElement('p')
-  timeText.classList.add('launch-time')
-  timeElement.appendChild(timetext)
-  //launch location
-  const locationElement = document.createElement('section')
-  locationElement.classList.add('launch-element')
-  locationElement.classList.add('location-element')
-  parent.appendChild(locationElement)
-
-  const locationLogo = document.createElement('i')
-  locationLogo.classList.add('fas fa-map-marked-alt')
-  locationElement.appendChild(locationLogo)
-
-  const locationText = document.createElement('p')
-  locationText.classList.add('launch-location')
-  locationElement.appendChild(locationText)
-}
-
 const getBackground = () => {
   fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
     .then(resp => {
@@ -124,9 +45,95 @@ const getLaunches = () => {
     })
     .then(launches => {
       for (let i = 0; i < launches.length; i++) {
-        launchArray[i] = launches[i]
+        launchArray.push(new Launch(launches[i]))
       }
     })
+}
+
+const navigate = direction => {
+  console.log(direction)
+  if (direction === 'right') {
+    if (launchIndex === launchArray.length - 1) {
+      launchIndex = 0
+    } else {
+      launchIndex++
+    }
+  } else if (direction === 'left') {
+    if (launchIndex === 0) {
+      launchIndex = launchArray.length - 1
+    } else {
+      launchIndex--
+    }
+  }
+  console.log(launchIndex)
+  render(launchIndex)
+}
+
+const render = index => {
+  const parent = document.querySelector('.launch-data')
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
+  }
+  //launch name
+  const nameElement = document.createElement('section')
+  nameElement.classList.add('launch-element')
+  nameElement.classList.add('name-element')
+  parent.appendChild(nameElement)
+
+  const nameLogo = document.createElement('i')
+  nameLogo.classList.add('fas')
+  nameLogo.classList.add('fa-space-shuttle')
+  nameElement.appendChild(nameLogo)
+
+  const nameText = document.createElement('h3')
+  nameText.classList.add('launch-name')
+  nameText.textContent = launchArray[index].launchName
+  nameElement.appendChild(nameText)
+  //launch descr
+  const descriptionElement = document.createElement('section')
+  descriptionElement.classList.add('launch-element')
+  descriptionElement.classList.add('description-element')
+  parent.appendChild(descriptionElement)
+
+  const descriptionLogo = document.createElement('i')
+  descriptionLogo.classList.add('fas')
+  descriptionLogo.classList.add('fa-info-circle')
+  descriptionElement.appendChild(descriptionLogo)
+
+  const descriptionText = document.createElement('p')
+  descriptionText.classList.add('launch-description')
+  descriptionText.textContent = launchArray[index].launchDescription
+  descriptionElement.appendChild(descriptionText)
+  //launch time
+  const timeElement = document.createElement('section')
+  timeElement.classList.add('launch-element')
+  timeElement.classList.add('time-element')
+  parent.appendChild(timeElement)
+
+  const timeLogo = document.createElement('i')
+  timeLogo.classList.add('fas')
+  timeLogo.classList.add('fa-clock')
+  timeElement.appendChild(timeLogo)
+
+  const timeText = document.createElement('p')
+  timeText.classList.add('launch-time')
+  timeText.textContent = launchArray[index].launchTime
+  timeElement.appendChild(timeText)
+  //launch location
+  const locationElement = document.createElement('section')
+  locationElement.classList.add('launch-element')
+  locationElement.classList.add('location-element')
+  parent.appendChild(locationElement)
+
+  const locationLogo = document.createElement('i')
+  locationLogo.classList.add('fas')
+  locationLogo.classList.add('fa-map-marked-alt')
+  locationElement.appendChild(locationLogo)
+
+  const locationText = document.createElement('p')
+  locationText.classList.add('launch-location')
+  locationText.textContent = launchArray[index].launchLocation
+  locationElement.appendChild(locationText)
 }
 
 document
